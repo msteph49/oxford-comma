993941f05817b11b370aed42122d9bbb80781a0b
Michael Stephens <mdstephens26@yahoo.com>
2020-11-21 16:40:18 -0500

    commit tree 9f31184e535dcfb654ffad5c256bbe5020b8cd2d

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+    if array.length == 1
+        string = array.join
+        string
+    elsif array.length == 2
+        string = array.join(" and ")
+        string
+    else array.length >= 3
+        string = array[0..-2].join(", ")
+        string << ", and"
+        string << " #{array[-1]}"
+        string
+    end
 end
